eb875d70e97d5f1498f952e6c748bc1d57cacf89
thepsalmist <xf.xavierfrank@gmail.com>
2021-12-02 06:04:44 +0300

    add account activation template

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class RegistrationView(View):
+    """
+    View to handle user registration
+    """
+
+    def get(self, request):
+        return render(request, "authentication/register.html")
+
+    def post(self, request):
+        return render(request, "authentication/register.html")
