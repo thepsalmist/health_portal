a5ea0fae7cb85342e60e36b628411ef9be37803d
thepsalmist <xf.xavierfrank@gmail.com>
2021-12-02 06:00:16 +0300

    login, logout auth templates

--- src/healthportal/settings/base.py
+++ src/healthportal/settings/base.py
@@ -102,7 +102,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = BASE_DIR / "static"
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
