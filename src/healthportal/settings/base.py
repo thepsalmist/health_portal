5b337824ac83e91ec989097bd9199099e6b18dee
thepsalmist <xf.xavierfrank@gmail.com>
2021-12-02 20:22:13 +0300

    add dashboard templates

--- src/healthportal/settings/base.py
+++ src/healthportal/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "authentication.apps.AuthenticationConfig",
+    "dashboard.apps.DashboardConfig",
 ]
 
 MIDDLEWARE = [
