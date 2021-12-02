586f01d62ab292c19157c40653d18a936875eccc
thepsalmist <xf.xavierfrank@gmail.com>
2021-12-02 05:00:15 +0300

    create authentication app

--- src/healthportal/settings/base.py
+++ src/healthportal/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=sgajc=$nlnw%tx8r(wd2xnu$%$bdun7z1#3#x&$8uuk*58(w5"
+SECRET_KEY = config("SECRET_KEY")
 
 # Application definition
 
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authentication.apps.AuthenticationConfig",
 ]
 
 MIDDLEWARE = [
